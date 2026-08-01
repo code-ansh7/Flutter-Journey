6e1b829ef18ad86825ed55d10b9ff53991d30e9a
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-08-01 12:17:39 +0545

    🎨 Day 03: Designed UI using Container, alignment and layout properties

--- Daily Learning App/hello_flutter_app/lib/main.dart
+++ Daily Learning App/hello_flutter_app/lib/main.dart
@@ -17,18 +17,47 @@ import 'package:flutter/material.dart';
 
 
 // Day - 02 
+// void main(){
+//   runApp(MyApp());
+// }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+
+//   @override
+//   Widget build(BuildContext context){
+//     return MaterialApp(
+//       home: Scaffold(
+//         body: const Center(   
+//           child: Text("Hello Ansh!\nFlutter Day 2 Complete 🚀"), // Yaha bhi const lga skte the!
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// Day - 03
+
 void main(){
   runApp(MyApp());
 }
-
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context){
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: const Center(   
-          child: Text("Hello Ansh!\nFlutter Day 1 Complete 🚀"), // Yaha bhi const lga skte the!
+        body: Center(
+          child: Container(
+            width: 200,
+            height: 100,
+            color: Colors.deepOrangeAccent,
+            alignment: Alignment.center,  // This Property is for Aligning.
+            child: const Text("Hello Ansh PAPA"),
+          ),
         ),
       ),
     );
