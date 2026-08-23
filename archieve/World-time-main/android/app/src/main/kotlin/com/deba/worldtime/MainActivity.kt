690c5e83556cbf90586c45c9c28a6c124f7b6c20
Ansh Rastogi <anshrastogi9236920967@gmail.com>
2026-08-23 23:14:22 +0545

    Refectoring

--- archieve/World-time-main/android/app/src/main/kotlin/com/deba/worldtime/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.deba.worldtime
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- archieve/motivation_app/android/app/src/main/kotlin/com/ansh/motivation_app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.ansh.motivation_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
